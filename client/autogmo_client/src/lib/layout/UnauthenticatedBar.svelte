2f44d4a2232bab31ffbf0a567675539cf195e834
Nathaniel King <nathanielarking@gmail.com>
2023-02-08 00:11:03 -0800

    started initial navbar structure

---
+++ client/autogmo_client/src/lib/layout/UnauthenticatedBar.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar gridColumns="grid-cols-3" padding="p-0" slotLead="justify-self-start" slotDefault="justify-self-center" slotTrail="justify-self-end">
+
+	<svelte:fragment slot="lead">
+        <a href="#">
+            <img class="w-12 mr-2 self-center" src="/autogmo_logo.png" alt="Logo" />
+        </a>
+        <div>
+            <a href="#" class="text-3xl no-underline hover:text-blue-dark font-sans font-bold">AutoGMO</a><br>
+            <span class="text-s text-grey-dark">Automated Garden Management & Optimization</span>
+        </div>
+    </svelte:fragment>
+        <ul class="">
+            <li class="">
+                <a href="#" class="btn rounded-none hover:bg-primary-500">Home</a>
+            </li>
+            <li>
+                <a href="#" class="btn rounded-none hover:bg-primary-500">Guides</a>
+            </li>
+            <li>
+                <a href="#" class="btn rounded-none hover:bg-primary-500">Github</a>
+            </li>
+            <li>
+                <a href="#" class="btn rounded-none hover:bg-primary-500">Donat</a>
+            </li>
+        </ul>
+    <svelte:fragment slot="trail">
+        <ul class="">
+            <li>
+                <a href="#" class="btn">Log in</a>
+            </li>
+            <li>
+                <a href="#" class="btn variant-filled-primary">Register</a>
+            </li>
+        </ul>
+    </svelte:fragment>
+
+
+</AppBar>
---
+++ client/autogmo_client/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../theme-garden.postcss';
+	import '@skeletonlabs/skeleton/styles/all.css';
+    import { AppShell } from '@skeletonlabs/skeleton';
+    import UnauthenticatedBar from '$lib/layout/UnauthenticatedBar.svelte';
+	import '../app.postcss';
+</script>
+<slot />
+
+<AppShell>
+	<svelte:fragment slot="header">
+        <UnauthenticatedBar/>
+    </svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
