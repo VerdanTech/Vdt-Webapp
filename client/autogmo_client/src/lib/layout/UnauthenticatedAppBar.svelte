2ac467caf436fe0510f203e1526c8c52983b8672
nathanielarking <nathanielarking@gmail.com>
2023-02-10 20:19:58 -0800

    Finished first draft of UnauthenticatedAppBar

---
+++ client/autogmo_client/src/lib/layout/UnauthenticatedAppBar.svelte
@@ -0,0 +1,69 @@
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+    <!--<AppBar gridColumns="grid-cols-3" gap="" padding="p-0" shadow="shadow-lg" slotLead="place-content-center" slotDefault="justify-self-center self-stretch" slotTrail="justify-self-stretch place-content-center">-->
+<AppBar gridColumns="grid-cols-3" padding="p-0" shadow="shadow-lg" slotLead="justify-self-center" slotDefault="justify-self-center self-stretch" slotTrail="justify-self-center">
+    <svelte:fragment slot="lead">
+        <nav class="font-semibold text-lg p-4">
+            <ul class="flex items-center">
+                <li>
+                    <a href="#">
+                        <img class="w-16 mr-4" src="/autogmo_logo.png" alt="Logo" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="">
+                        <span>AutoGMO</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </svelte:fragment>
+	<svelte:fragment slot="default">
+        <nav class="h-full font-semibold text-lg">
+            <ul class="h-full flex">
+                <li class="p-4 hover:bg-primary-700 hover:text-on-primary-token transition">
+                    <a href="#" class="h-full flex">
+                        <span class="self-center p-4">
+                            Guides
+                        </span>
+                    </a>
+                </li>
+                <li class="p-4 hover:bg-primary-700 hover:text-on-primary-token transition">
+                    <a href="#" class="h-full flex">
+                        <span class="self-center p-4">
+                            Github
+                        </span>
+                    </a>
+                </li>
+                <li class="p-4 hover:bg-primary-700 hover:text-on-primary-token transition">
+                    <a href="#" class="h-full flex">
+                        <span class="self-center p-4">
+                            About
+                        </span>
+                    </a>
+                </li>
+                <li class="p-4 hover:bg-primary-700 hover:text-on-primary-token transition">
+                    <a href="#" class="h-full flex">
+                        <span class="self-center p-4">
+                            Donate
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </svelte:fragment>
+	<svelte:fragment slot="trail">
+        <nav class="font-semibold text-lg">
+            <ul class="flex items-center">
+                <li class="m-2">
+                    <a href="#" class="btn">Log in</a>
+                </li>
+                <li class="m-2">
+                    <a href="#" class="btn variant-filled-primary">Get started</a>
+                </li>
+            </ul>
+        </nav>
+    </svelte:fragment>
+</AppBar>
--- client/autogmo_client/src/routes/+layout.svelte
+++ client/autogmo_client/src/routes/+layout.svelte
@@ -2,21 +2,15 @@
 	import '../theme-garden.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
     import { AppShell } from '@skeletonlabs/skeleton';
-    import UnauthenticatedBar from '$lib/layout/UnauthenticatedBar.svelte';
+    import UnauthenticatedAppBar from '$lib/layout/UnauthenticatedAppBar.svelte';
 	import '../app.postcss';
 </script>
-<slot />
 
 <AppShell>
 	<svelte:fragment slot="header">
-        <UnauthenticatedBar/>
+        <UnauthenticatedAppBar/>
     </svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
-	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
-	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
 	<!-- Router Slot -->
 	<slot />
 	<!-- ---- / ---- -->
-	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-	<svelte:fragment slot="footer">Footer</svelte:fragment>
 </AppShell>
