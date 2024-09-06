ef08283dbcf83d403dcb4198201a4dbf94b14025
Nathaniel <nathanielarking@gmail.com>
2024-09-06 17:51:07 +0000

    Added debounce function

---
+++ frontend/src/lib/utils/debounce.ts
@@ -0,0 +1,12 @@
+/**
+ * Returns a debounced version of the provided callback function.
+ * Taken from https://www.okupter.com/blog/svelte-debounce
+ */
+export const debounce = (callback: Function, wait = 300) => {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    return (...args: any[]) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => callback(...args), wait);
+    };
+};
